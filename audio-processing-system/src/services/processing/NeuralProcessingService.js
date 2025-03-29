961f589da92ed70544ffb56d60c97a8efaeff83e
deskredchairblue <desk.is.red@outlook.com>
2025-03-29 18:53:48 -0400

    Delete audio-processing-system directory

--- audio-processing-system/src/services/processing/NeuralProcessingService.js
+++
@@ -1,9 +0,0 @@
-// src/services/processing/NeuralProcessingService.js
-class NeuralProcessingService {
-  async enhance(buffer, model = 'basic-vocal-cleaner') {
-    // Placeholder: simulate ML enhancement
-    return Buffer.from(buffer); // return the same buffer for now
-  }
-}
-
-module.exports = NeuralProcessingService;
--- audio-processing-system/src/utils/helpers/AudioEventEmitter.js
+++
@@ -1,6 +0,0 @@
-// src/utils/helpers/AudioEventEmitter.js
-const EventEmitter = require('events');
-
-class AudioEventEmitter extends EventEmitter {}
-
-module.exports = new AudioEventEmitter();
